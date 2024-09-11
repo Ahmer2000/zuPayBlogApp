6f01aab2c58960d98d09278dc189908b45a06603
Ahmer Sayyed <ahmersayyed2000@gmail.com>
2024-09-11 16:51:49 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,31 @@
-import { config } from 'dotenv';
-import { resolve } from 'path';
-config({path:resolve(__dirname,'../.env')});
-import express, { json, static as static_tatic } from 'express';
-import cors from 'cors';
-import { connect } from 'mongoose';
-import { findOne, create } from './models/User';
-import { create as _create, find, findById, findByIdAndDelete } from './models/Post';
-import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
-import { sign, verify } from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
-import multer from 'multer';
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path:path.resolve(__dirname,'../.env')});
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const Post = require('./models/Post');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
 const uploadMiddleware = multer({dest:'uploads/'})
-import { renameSync } from 'fs';
+const fs = require('fs');
 
 const app = express();
 
-const salt = genSaltSync(10);
+const salt = bcrypt.genSaltSync(10);
 const secret = process.env.JWT_SECRET;
 
 //MIDDLEWARES
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
-app.use(json());
+app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads',static_tatic(__dirname + '/uploads'));
+app.use('/uploads',express.static(__dirname + '/uploads'));
 
 let port = process.env.PORT || 3001;
-connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 // console.log(process.env.MONGO_URI);
 
 
@@ -33,9 +33,9 @@ connect(process.env.MONGO_URI);
 app.post('/register',async (req,res)=>{
     const{username,password} = req.body;
     try {
-        const compareUser = await findOne({username})
+        const compareUser = await User.findOne({username})
         if(username.length >= 4 && !compareUser){
-        await create({username,password:hashSync(password,salt)})
+        await User.create({username,password:bcrypt.hashSync(password,salt)})
         }
         else{
             res.status(400).json('minimum 4 letters are required.')
@@ -51,11 +51,11 @@ app.post('/register',async (req,res)=>{
 app.post('/login',async(req,res)=>{
     const{username,password} = req.body;
     try {
-        const grabUser = await findOne({username});
-        const comparePassword = compareSync(password,grabUser.password);
+        const grabUser = await User.findOne({username});
+        const comparePassword = bcrypt.compareSync(password,grabUser.password);
         if(comparePassword){
             //logged in. 
-            sign({username,id:grabUser._id},secret,{},(err,token)=>{
+            jwt.sign({username,id:grabUser._id},secret,{},(err,token)=>{
                 if(err) throw err;
                 res.cookie('token',token).json({
                     id:grabUser._id,
@@ -79,7 +79,7 @@ app.post('/login',async(req,res)=>{
 //Endpoint for verifying the jwt token of the user currently logged in.
 app.get('/profile',(req,res)=>{
     const{token} = req.cookies;
-    verify(token,secret,{},(err,userInfo)=>{
+    jwt.verify(token,secret,{},(err,userInfo)=>{
         if (err) throw err;
         res.json(userInfo);
     });
@@ -97,21 +97,21 @@ app.post('/createNewPost', uploadMiddleware.single('file') , async(req,res)=>{
     const parts = originalname.split('.');
     const ext = parts[parts.length-1];
     const newPath = path + '.' + ext;
-    renameSync(path,newPath);
+    fs.renameSync(path,newPath);
 
     const{token} = req.cookies;
-    verify(token,secret,{}, async(err,userInfo)=>{
+    jwt.verify(token,secret,{}, async(err,userInfo)=>{
         if (err) throw err;
         //Saving the blog post in the database.
         const{title,summary,content} = req.body;
-        const postDoc = await _create({title,summary,content,blogImg:newPath,author:userInfo.id});
+        const postDoc = await Post.create({title,summary,content,blogImg:newPath,author:userInfo.id});
         res.json(postDoc);
     });
 })
 
 //Endpoint for creating a post of the currently logged in user. 
 app.get('/createNewPost',async(req,res)=>{
-        const posts = await find().populate('author',['username']).sort({createdAt:-1});
+        const posts = await Post.find().populate('author',['username']).sort({createdAt:-1});
         res.json(posts);   
 })
 
@@ -123,13 +123,13 @@ app.put('/createNewPost', uploadMiddleware.single('file'), async(req,res)=>{
         const parts = originalname.split('.');
         const ext = parts[parts.length-1];
         newPath = path + '.' + ext;
-        renameSync(path,newPath);
+        fs.renameSync(path,newPath);
     }
     const{token} = req.cookies;
-    verify(token,secret,{}, async(err,userInfo)=>{
+    jwt.verify(token,secret,{}, async(err,userInfo)=>{
         if (err) throw err;
         const{title,summary,content,id} = req.body;
-        const postDoc = await findById(id);
+        const postDoc = await Post.findById(id);
         const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(userInfo.id);
         //Updating the blog post in the database.
         if (!isAuthor) {
@@ -150,14 +150,14 @@ app.put('/createNewPost', uploadMiddleware.single('file'), async(req,res)=>{
 //Endpoint for displaying the selected post in full page mode. 
 app.get('/post/:id', async(req,res)=>{
     const{id} = req.params;
-    const postDoc = await findById(id).populate('author',['username']);
+    const postDoc = await Post.findById(id).populate('author',['username']);
     res.json(postDoc);
 })
 
 //Endpoint for deleting a post from the database.
 app.delete('/deletePost/:id', async (req,res)=>{
     const {id} = req.params;
-    await findByIdAndDelete(id);
+    await Post.findByIdAndDelete(id);
 })
 
 app.listen(port)
