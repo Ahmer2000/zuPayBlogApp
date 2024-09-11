55f28d78ecf834ddf30d537c02d7455a48242aa1
Ahmer Sayyed <ahmersayyed2000@gmail.com>
2024-09-11 15:50:57 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname + '/uploads'));
 
-let port = process.env.PORT||7000;
+let port = process.env.PORT || 4001;
 mongoose.connect(process.env.MONGO_URI);
 // console.log(process.env.MONGO_URI);
 
